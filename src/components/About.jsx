24722fff90587c13c2fad6ed5f2c1df1c85594bd
khushichetule09 <khushichetule2@gmail.com>
2025-08-07 15:02:38 +0530

    Create responsive about section for restaurant website

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import img from "../assets/img/about.png";
+import Button from "../layouts/Button";
+
+const About = () => {
+  return (
+    <div className="min-h-screen flex flex-col lg:flex-row justify-center items-center lg:px-32 px-5">
+      <img src={img} alt="img" />
+
+      <div className=" space-y-4 lg:pt-14">
+        <h1 className=" font-semibold text-4xl text-center md:text-start">
+          Why Choose Us?
+        </h1>
+        <p>
+         we serve food that you want to eat that take your mouth watering and you don't get bore of this food so get a proper taisty food from our restaurant!!
+        </p>
+        <p>
+         We serve a love with food for you all plese visit our place for good flavour!!
+        </p>
+        <div className=" flex justify-center lg:justify-start">
+          <Button title="Learn More" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
